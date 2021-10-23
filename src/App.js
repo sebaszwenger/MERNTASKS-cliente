4f2ffb07497e1cc7912a9e5d1946fe7dceea4f83
sebaszwenger <sebaszwenger@gmail.com>
2021-10-23 20:47:47 -0300

    registering routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import NewAccount from "./components/auth/NewAccount";
+import Proyects from "./components/proyects/proyects";
+
 function App() {
   return (
-    <div>
-      <h1>merntasks</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/new-account" component={NewAccount} />
+        <Route exact path="/proyects" component={Proyects} />
+      </Switch>
+    </Router>
   );
 }
 
