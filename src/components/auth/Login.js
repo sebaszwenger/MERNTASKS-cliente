27436543952f46eac96c89e489c04d6db3849e5f
sebaszwenger <sebaszwenger@gmail.com>
2021-10-23 21:00:17 -0300

    add inputs email & password. add submit button.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,46 @@
 import React from "react";
 
 const Login = () => {
-  return <h1>from Login</h1>;
+  const onChange = () => {};
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Log in</h1>
+
+        <form>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              name="email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Your Password"
+              name="password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Log in"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
