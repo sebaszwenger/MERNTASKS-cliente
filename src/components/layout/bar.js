dc6771cfd64e505909c9e6568ada568d22717425
sebaszwenger <sebaszwenger@gmail.com>
2021-10-30 12:55:51 -0300

    add new layout component, Bar

---
+++ src/components/layout/bar.js
@@ -0,0 +1,15 @@
+const Bar = () => {
+  return (
+    <header className="app-header">
+      <p className="nombre-usuario">
+        Hola <span>Sebas</span>
+      </p>
+
+      <nav className="nav-principal">
+        <a href="#!">Sign off</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Bar;
