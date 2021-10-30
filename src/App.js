74fde2ecf80614ee371301dec6ba3ec957b8727b
sebaszwenger <sebaszwenger@gmail.com>
2021-10-30 12:51:46 -0300

    hotfix name of directory & component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import NewAccount from "./components/auth/NewAccount";
-import Proyects from "./components/proyects/proyects";
+import Proyects from "./components/projects/projects";
 
 function App() {
   return (
