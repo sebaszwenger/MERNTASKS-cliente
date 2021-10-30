c475491ffea5f1525ea7f1394687633a3778710e
sebaszwenger <sebaszwenger@gmail.com>
2021-10-30 12:58:29 -0300

    new component Project to show each project

---
+++ src/components/projects/project.js
@@ -0,0 +1,11 @@
+const Project = ({ project }) => {
+  return (
+    <li>
+      <button type="button" className="btn btn-blank">
+        {project.name}
+      </button>
+    </li>
+  );
+};
+
+export default Project;
